4190805b03074dae9c94d028f867e5617b3c5e85
maozhijian <chiefmaster@163.com>
2013-09-06 13:50:13 +0800

    save

---
+++ external/widgets/test_mao/widget.info.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'name'      => 'test_mao',
+    'display_name'  => 'mao',
+    'author'    => 'carrymao',
+    'website'   => 'http://runmall.com',
+    'version'   => '1.0',
+    'desc'      => '',
+    'configurable'  => false,
+);
+
+?>
